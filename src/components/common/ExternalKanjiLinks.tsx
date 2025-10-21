07f747015ba508c3a2e0cb796af04ea0560b463d
Mithi Sevilla <mithi.sevilla@gmail.com>
2025-10-21 18:50:09 +0800

    Add utm source

--- src/components/common/ExternalKanjiLinks.tsx
+++ src/components/common/ExternalKanjiLinks.tsx
@@ -7,7 +7,10 @@ export const ExternalKanjiLinks = ({ kanji }: { kanji: string }) => {
       {externalLinks.map((item) => {
         return (
           <li key={item.name} className="m-1">
-            <ExternalTextLink href={item.url(kanji)} text={item.name} />
+            <ExternalTextLink
+              href={`${item.url(kanji)}/?utm_source=kanjiheatmap`}
+              text={item.name}
+            />
           </li>
         );
       })}
