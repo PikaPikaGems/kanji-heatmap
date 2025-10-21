8e22efcb60e1b6e01af21e5d5efbb90ace3743d1
Mithi Sevilla <mithi.sevilla@gmail.com>
2025-10-21 18:40:30 +0800

    Update padding for 404 page

--- src/components/error/KanjiNotFound.tsx
+++ src/components/error/KanjiNotFound.tsx
@@ -5,12 +5,14 @@ import { Sumimasen } from "./Sumimasen";
 export const KanjiNotFound = ({ kanji }: { kanji: string }) => {
   return (
     <Wrapper>
-      <Sumimasen />
-      <div className="my-2 font-bold">{`"${kanji}" Not Found`}</div>
-      <div className="text-xs max-w-96 py-2  my-2 text-left">
-        <span>You can try looking for it in the following places:</span>
-        <div className="flex flex-col items-center justify-center my-2">
-          <ExternalKanjiLinks kanji={kanji} />
+      <div className="px-4">
+        <Sumimasen />
+        <div className="my-2 font-bold">{`"${kanji}" Not Found`}</div>
+        <div className="text-xs max-w-96 py-2  my-2 text-left">
+          <span>You can try looking for it in the following places:</span>
+          <div className="flex flex-col items-center justify-center my-2">
+            <ExternalKanjiLinks kanji={kanji} />
+          </div>
         </div>
       </div>
     </Wrapper>
