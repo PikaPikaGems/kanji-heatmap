c53d89ccdabf51b791a04fd120bd8775deccdf5a
Mithi Sevilla <mithi.sevilla@gmail.com>
2026-02-05 22:29:48 +0800

    Add more colors, change default color to blue

--- src/hooks/use-change-theme-color.ts
+++ src/hooks/use-change-theme-color.ts
@@ -4,12 +4,24 @@ const LOCAL_STORAGE_THEME_COLOR_KEY = "theme-color";
 
 // Only store RGB values
 export const themeColorsRgb = [
-  "251, 2, 168", // pink
-  "25, 60, 184", // blue
-  "170, 0, 255", // purple
-  "0, 170, 255", // light blue
-  "236, 0, 63", // red
-  "0, 166, 62", // green
+  "25, 60, 184", // blue (blue-700)
+  "251, 2, 168", // pink (fuchsia-600)
+  "170, 0, 255", // purple (purple-600)
+  "0, 170, 255", // light blue (sky-500)
+  "236, 0, 63", // red (rose-600)
+  "0, 166, 62", // green (green-600)
+  "234, 88, 12", // orange (orange-600)
+  "234, 179, 8", // yellow (yellow-500)
+  "20, 184, 166", // teal (teal-500)
+  "168, 85, 247", // violet (violet-500)
+  "236, 72, 153", // pink (pink-500)
+  "6, 182, 212", // cyan (cyan-500)
+  "132, 204, 22", // lime (lime-500)
+  "251, 146, 60", // amber (amber-400)
+  "244, 63, 94", // rose (rose-500)
+  "59, 130, 246", // blue (blue-500)
+  "14, 165, 233", // sky (sky-500)
+  "99, 102, 241", // indigo (indigo-500)
 ];
 
 // rgbToHex(color)
