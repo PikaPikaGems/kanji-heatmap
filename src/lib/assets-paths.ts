c4cea0cba4406ea82dea0c421940d954b1ef0014
Mithi Sevilla <mithi.sevilla@gmail.com>
2026-03-30 22:17:57 +0800

    v3 of vocab

--- src/lib/assets-paths.ts
+++ src/lib/assets-paths.ts
@@ -7,10 +7,10 @@ const assetsPaths = {
   VOCAB_FURIGANA: "/json/vocab_furigana.json",
   CUM_USE: "/json/cum_use.json",
   KANJI_SVGS: "https://assets.pikapikagems.com/kanji/",
-  KANJI_VOCAB: "https://assets.pikapikagems.com/kanji-common-words/v2", // Note: No slash at the end is intentional
+  KANJI_VOCAB: "https://assets.pikapikagems.com/kanji-common-words/v3", // Note: No slash at the end is intentional
   dev: {
     KANJI_SVGS: "https://kanjivg.tagaini.net/kanjivg/kanji/",
-    KANJI_VOCAB: "/kanji-words/v2", // Note: No slash at the end is intentional
+    KANJI_VOCAB: "/kanji-words/v3", // Note: No slash at the end is intentional
   },
   KANJI_DECOMPOSITION: "/json/kanji_decomposition.json",
   KANJI_READING_DETAILS: "/json/kanji-readings-details-filtered.json",
