548d8591fcf60c2d0b53558bdebd6272253a9bba
Mithi Sevilla <mithi.sevilla@gmail.com>
2026-06-10 00:37:17 +0800

    Improve dialog css for sort and filters

--- src/components/screens/ListScreen/ControlBar/SortAndFilter/SortAndFilterForm.tsx
+++ src/components/screens/ListScreen/ControlBar/SortAndFilter/SortAndFilterForm.tsx
@@ -61,7 +61,7 @@ export const SortAndFilterSettingsForm = ({
 
   return (
     <form
-      className="flex flex-col space-y-4"
+      className="flex flex-col flex-1 min-h-0"
       onSubmit={(e) => {
         e.preventDefault();
         onSettle({
@@ -71,168 +71,175 @@ export const SortAndFilterSettingsForm = ({
         });
       }}
     >
-      <SortOrderSectionLayout
-        primaryField={
-          <>
-            <ResponsiveSelect
-              value={sortValues.primary}
-              onChange={(newValue) => {
-                const isGroup = (GROUP_OPTIONS as readonly string[]).includes(
-                  newValue
-                );
-
-                setSortValues((prev) => {
-                  const newSecondary =
-                    newValue === prev.secondary
-                      ? "none"
-                      : isGroup
-                        ? prev.secondary
-                        : "none";
-                  return {
-                    ...prev,
-                    primary: newValue as SortKey,
-                    secondary: newSecondary,
-                  };
-                });
-              }}
-              options={SORT_ORDER_SELECT}
-              label="Primary"
-            />
-            <FreqRankTypeInfo value={sortValues.primary} defaultValue={null} />
-          </>
-        }
-        secondaryField={
-          sortValues.secondary &&
-          isGroup && (
+      <div className="flex-1 min-h-0 px-2 space-y-4 overflow-y-auto">
+        <SortOrderSectionLayout
+          primaryField={
             <>
               <ResponsiveSelect
-                value={sortValues.secondary}
-                onChange={(newValue) =>
-                  setSortValues((prev) => {
-                    return { ...prev, secondary: newValue as SortKey };
-                  })
-                }
-                options={SORT_ORDER_SELECT.filter((item) => {
-                  return item.value !== sortValues.primary;
-                })}
-                label="Secondary"
-              />
-              <FreqRankTypeInfo
-                value={sortValues.secondary}
-                defaultValue={null}
-              />
-            </>
-          )
-        }
-        additionalInfo={
-          <SortAdditionalInfo
-            val1={sortValues.primary}
-            val2={sortValues.secondary}
-          />
-        }
-      />
+                value={sortValues.primary}
+                onChange={(newValue) => {
+                  const isGroup = (GROUP_OPTIONS as readonly string[]).includes(
+                    newValue
+                  );
 
-      <FilterSectionLayout
-        strokeCountField={
-          <StrokeCountField
-            values={[
-              filterValues.strokeRange.min,
-              filterValues.strokeRange.max,
-            ]}
-            setValues={(val) => {
-              setFilterValues((prev) => {
-                return {
-                  ...prev,
-                  strokeRange: {
-                    min: val[0] ?? 0,
-                    max: val[1] ?? MAX_STROKE_COUNT,
-                  },
-                };
-              });
-            }}
-          />
-        }
-        jlptField={
-          <JLPTSelector
-            selectedJLPT={filterValues.jlpt}
-            setSelectedJLPT={(val) => {
-              setFilterValues((prev) => {
-                return { ...prev, jlpt: val };
-              });
-            }}
-          />
-        }
-        freqRankSourceField={
-          <FrequencyRankDataSource
-            value={filterValues.freq.source}
-            setValue={(val) => {
-              setFilterValues((prev) => {
-                return {
-                  ...prev,
-                  freq: {
-                    ...prev.freq,
-                    source: val as FrequencyType,
-                    rankRange:
-                      val === "none"
-                        ? { min: 1, max: MAX_FREQ_RANK }
-                        : prev.freq.rankRange,
-                  },
-                };
-              });
-            }}
-          />
-        }
-        freqRankRangeField={
-          filterValues.freq.source !== "none" && (
-            <FrequencyRankingRangeField
+                  setSortValues((prev) => {
+                    const newSecondary =
+                      newValue === prev.secondary
+                        ? "none"
+                        : isGroup
+                          ? prev.secondary
+                          : "none";
+                    return {
+                      ...prev,
+                      primary: newValue as SortKey,
+                      secondary: newSecondary,
+                    };
+                  });
+                }}
+                options={SORT_ORDER_SELECT}
+                label="Primary"
+              />
+              <FreqRankTypeInfo value={sortValues.primary} defaultValue={null} />
+            </>
+          }
+          secondaryField={
+            sortValues.secondary &&
+            isGroup && (
+              <>
+                <ResponsiveSelect
+                  value={sortValues.secondary}
+                  onChange={(newValue) =>
+                    setSortValues((prev) => {
+                      return { ...prev, secondary: newValue as SortKey };
+                    })
+                  }
+                  options={SORT_ORDER_SELECT.filter((item) => {
+                    return item.value !== sortValues.primary;
+                  })}
+                  label="Secondary"
+                />
+                <FreqRankTypeInfo
+                  value={sortValues.secondary}
+                  defaultValue={null}
+                />
+              </>
+            )
+          }
+          additionalInfo={
+            <SortAdditionalInfo
+              val1={sortValues.primary}
+              val2={sortValues.secondary}
+            />
+          }
+        />
+
+        <FilterSectionLayout
+          strokeCountField={
+            <StrokeCountField
               values={[
-                filterValues.freq.rankRange.min,
-                filterValues.freq.rankRange.max,
+                filterValues.strokeRange.min,
+                filterValues.strokeRange.max,
               ]}
               setValues={(val) => {
                 setFilterValues((prev) => {
                   return {
                     ...prev,
-                    freq: {
-                      ...prev.freq,
-                      rankRange: {
-                        min: val[0] ?? 1,
-                        max: val[1] ?? MAX_FREQ_RANK,
-                      },
+                    strokeRange: {
+                      min: val[0] ?? 0,
+                      max: val[1] ?? MAX_STROKE_COUNT,
                     },
                   };
                 });
               }}
             />
-          )
-        }
-      />
-      {!isDisabled && (
-        <ItemCount
-          settings={{ ...initialValue, filterSettings: filterValues }}
+          }
+          jlptField={
+            <JLPTSelector
+              selectedJLPT={filterValues.jlpt}
+              setSelectedJLPT={(val) => {
+                setFilterValues((prev) => {
+                  return { ...prev, jlpt: val };
+                });
+              }}
+            />
+          }
+          freqRankSourceField={
+            <FrequencyRankDataSource
+              value={filterValues.freq.source}
+              setValue={(val) => {
+                setFilterValues((prev) => {
+                  return {
+                    ...prev,
+                    freq: {
+                      ...prev.freq,
+                      source: val as FrequencyType,
+                      rankRange:
+                        val === "none"
+                          ? { min: 1, max: MAX_FREQ_RANK }
+                          : prev.freq.rankRange,
+                    },
+                  };
+                });
+              }}
+            />
+          }
+          freqRankRangeField={
+            filterValues.freq.source !== "none" && (
+              <FrequencyRankingRangeField
+                values={[
+                  filterValues.freq.rankRange.min,
+                  filterValues.freq.rankRange.max,
+                ]}
+                setValues={(val) => {
+                  setFilterValues((prev) => {
+                    return {
+                      ...prev,
+                      freq: {
+                        ...prev.freq,
+                        rankRange: {
+                          min: val[0] ?? 1,
+                          max: val[1] ?? MAX_FREQ_RANK,
+                        },
+                      },
+                    };
+                  });
+                }}
+              />
+            )
+          }
         />
-      )}
-      {isDisabled && (
-        <div className="flex w-full justify-end items-center text-xs">
-          There are no changes to apply yet.
-        </div>
-      )}
-      <div className="flex justify-end border-t pt-3 px-0 space-x-1">
-        <Button
-          variant="outline"
-          onClick={(e) => {
-            e.preventDefault();
-            setSortValues(defaultSortSettings);
-            setFilterValues(defaultFilterSettings);
-            e.stopPropagation();
-          }}
-        >
-          Clear all
-        </Button>
-        <Button disabled={isDisabled} type="submit">
-          Apply
-        </Button>
+
       </div>
+      <div className="px-2 pt-2 border-t">
+        {!isDisabled && (
+          <ItemCount
+            settings={{ ...initialValue, filterSettings: filterValues }}
+          />
+        )}
+        {isDisabled && (
+          <div className="flex items-center justify-end w-full text-xs">
+            There are no changes to apply yet.
+          </div>
+        )}
+        <div className="flex justify-end px-0 pt-2 space-x-1">
+
+          <Button
+            variant="outline"
+            onClick={(e) => {
+              e.preventDefault();
+              setSortValues(defaultSortSettings);
+              setFilterValues(defaultFilterSettings);
+              e.stopPropagation();
+            }}
+          >
+            Clear all
+          </Button>
+          <Button disabled={isDisabled} type="submit">
+            Apply
+          </Button>
+        </div>
+      </div>
+
     </form>
   );
 };
