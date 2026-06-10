a75409e143bbcfb46089c39d3a76775eae834b1b
Mithi Sevilla <mithi.sevilla@gmail.com>
2026-06-10 22:04:15 +0800

    New error screen

--- src/components/error/SmallUnexpectedErrorFallback.tsx
+++ src/components/error/SmallUnexpectedErrorFallback.tsx
@@ -3,10 +3,21 @@ import { ExternalTextLink } from "../common/ExternalTextLink";
 
 export const SmallUnexpectedErrorFallback = () => {
   return (
-    <div className="w-full text-xs h-full font-bold flex justify-center items-center p-2">
+    <div className="flex items-center justify-center w-full h-full p-2 text-xs font-bold">
       <div>
         すみません 🙇🏽‍♀️ 🙇. <br /> Report error on
         <ExternalTextLink text="Discord." href={outLinks.discord} />
+      </div>
+    </div>
+  );
+};
+
+
+export const SmallUnexpectedErrorFallbackTxt = ({ txt }: { txt: string }) => {
+  return (
+    <div className="flex items-center justify-center w-full h-full p-2 text-xs font-bold">
+      <div>
+        すみません 🙇🏽‍♀️ 🙇. <br /> {txt}
       </div>
     </div>
   );
